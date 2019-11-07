4c95f6f795656f90b41ed42c353d98c436eb8f8b
ilham17 <ilhammttqn2@gmail.com>
2019-11-08 06:07:11 +0700

    tambah file

---
+++ test/kategori.php
@@ -0,0 +1,30 @@
+<?php
+
+include "layout/header.php";
+$id = $_GET['id'];
+
+$query = mysql_query("SELECT * FROM berita where id_kategori = '$id' order by id_berita DESC",$con);
+$data = mysql_fetch_assoc($query);
+
+?>
+<?php do{ ?>
+<div class="grid">
+	<div class="cover-lol">
+		<img src="upload/<?=$data['gambar_berita'];?>" width="200px">
+	</div>
+	<div class="judul">
+		<h5><a href="lihat_berita.php?id=<?=$data['id_berita'];?>"><?=$data['judul_berita'];?></a></h5>
+	</div>
+	<div>
+		<ul>
+			<li><?=tanggal($data['tanggal_berita']) ;?></li>
+		</ul>		
+	</div>
+	<div class="tombol-lihat">
+		<a href="lihat_berita.php?id=<?=$data['id_berita'];?>">LIHAT</a>
+	
+	</div>
+</div>
+<?php }while($data = mysql_fetch_assoc($query));?>
+
+<?php require"layout/footer.php"; ?>
